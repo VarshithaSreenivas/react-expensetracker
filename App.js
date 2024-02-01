441216685d9e2d8726ea792b07082271ef0d57ec
VarshithaSreenivas <87645604+VarshithaSreenivas@users.noreply.github.com>
2024-02-01 17:44:34 +0530

    Add files via upload
    waiter app app.js

--- App.js
+++ App.js
@@ -1,44 +1,23 @@
-import ExpenseItem from "./components/ExpenseItem";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location:'delhi',
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12),location:'AP' },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location:'TN',
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location:'goa',
-    },
-  ];
   return (
-    <div>
-      <h1>Lets get Started</h1>
-      <div>
-        {expenses.map(expense => (
-          <ExpenseItem
-            key={expense.id}  
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-            location={expense.location}
-          />
-        ))}
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
